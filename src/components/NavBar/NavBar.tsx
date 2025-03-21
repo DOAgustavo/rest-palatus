39b1b1013f7bc334641f9ed07ebd83821c93b2d4
DOAgustavo <givoannigustavos@gmail.com>
2025-03-21 05:27:53 -0300

    verificar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,7 +3,9 @@ import NavButton from '../buttons/NavButton';
 
 export default function NavBar() {
   return (
-    <div className="navbar bg-brown px-3 text-white">
+   
+    
+    <header  className="navbar bg-brown px-3 text-white">
 
       
       <div className="container d-flex flex-wrap justify-content-between align-items-center">
@@ -13,12 +15,12 @@ export default function NavBar() {
             <FaHome size={30} className="me-2" />
           </a>
         </div>
-      <NavButton />
+      
         {/* Links */}
         <section>
-          <ul className="d-flex flex-wrap list-unstyled m-0 justify-content-center">
-            <li className="mx- mb-1 mb-md-0">
-              <a href="#">Sobre nós</a>
+          <ul className="d-flex flex-wrap list-unstyled m-0 justify-content-center ">
+            <li className="mx- mb-1 mb-md-0 ">
+              <a className='' href="#">Sobre nós</a>
             </li>
             <li className="mx-5 mb-1 mb-md-0">
               <a href="#">Cardápio</a>
@@ -37,8 +39,12 @@ export default function NavBar() {
               </a>
             </li>
           </ul>
+          
         </section>
+        
       </div>
-    </div>
+      <NavButton/>
+    </header>
+    
   );
 }
--- src/components/buttons/NavButton.tsx
+++ src/components/buttons/NavButton.tsx
@@ -3,8 +3,28 @@
 import Hamburger from "hamburger-react";
 import { useState } from "react";
 export default function NavButton() {
-    const [Open, setOpen] = useState(false);
-    return (<div>
-        <Hamburger toggled={Open} toggle={setOpen} size={20} />
-    </div>);
+    const [open, setOpen] = useState(false);
+    return <header>
+        
+       
+       
+        {open &&  <div className=" navbutton absolute top-0 left-0 w-full h-full bg-white text-black z-50 flex items-center justify-center">
+                    
+            
+            <ul className="d-flex flex-wrap list-unstyled m-0 justify-content-center ">
+                        <li className="mx- mb-1 mb-md-0 ">
+                          <a className='' href="#">Sobre nós</a>
+                        </li>
+                        <li className="mx-5 mb-1 mb-md-0">
+                          <a href="#">Cardápio</a>
+                        </li>
+                        <li className="mx-5 mb-1 mb-2 ms-0 mb-md-0">
+                          <a href="#">Delivery</a>
+                        </li>
+                       
+                      </ul>
+        </div>}
+        <Hamburger toggled={open} toggle={setOpen} />
+        </header>
+    
 }
