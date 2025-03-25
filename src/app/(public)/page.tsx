3c4d104e1f7754b646bf071e8494c1747cb17ee9
GIOVANNI GUSTAVO SANTOS SOUSA <giovanni_sousa@estudante.sesisenai.org.br>
2025-03-25 15:40:42 -0300

    funcionando

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -6,6 +6,7 @@ import { FaWhatsapp, FaInstagram } from "react-icons/fa";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+
 export default function HomePage() {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -38,35 +39,78 @@ export default function HomePage() {
               Palatus
             </a>
           </h2>
-          <button className="nav-button fa fa-bars" onClick={handleNavButtonClick}>  ☰</button>
-<div className={menuOpen ? "show" : ""}>
-<ul>
-  <button className="exit-menu fa fa-times" onClick={handleExitMenuClick}></button>
-            
+          <button
+            className="nav-button fa fa-bars"
+            onClick={handleNavButtonClick}
+          >
+            ☰
+          </button>
+          <div className={menuOpen ? "show" : ""}>
+            <ul>
+              <button
+                className="exit-menu fa fa-times"
+                onClick={handleExitMenuClick}
+              ></button>
+
               <li>
                 <a href="#" className="active">
                   Bem Vindo
                 </a>
               </li>
               <li>
-                <a href="#">Sobre Nós</a>
-              </li>
-              <li>
-                <a href="#">Menu</a>
-              </li>
-              <li>
-                <a href="#">Delivery</a>
-              </li>
-<li>
-                <a href="">
-                  <FaWhatsapp size={20} />
+                <a
+                  href="#about-section"
+                  onClick={() => {
+                    const aboutSection =
+                      document.getElementById("about-section");
+                    if (aboutSection) {
+                      aboutSection.scrollIntoView({
+                        behavior: "smooth",
+                        block: "center",
+                      });
+                    }
+                    setMenuOpen(false); // Fecha o menu após o clique
+                  }}
+                >
+                  Sobre Nós
                 </a>
               </li>
               <li>
-                <a href="">
-                  <FaInstagram size={20} />
+                <a
+                  href="#"
+                  onClick={() => {
+                    const menuSection =
+                      document.getElementById("menu-section");
+                    if (menuSection) {
+                      menuSection.scrollIntoView({
+                        behavior: "smooth",
+                        block: "center",
+                      });
+                    }
+                    setMenuOpen(false); // Fecha o menu após o clique
+                  }}
+                >
+                  Menu
                 </a>
-              
+              </li>
+
+              <li>
+              <a
+  href="https://wa.me/554797090916"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <FaWhatsapp size={25} />
+</a>
+              </li>
+              <li>
+              <a
+  href="https://www.instagram.com/romaniepalatuspizzaria/"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <FaInstagram size={25} />
+</a>
               </li>
             </ul>
           </div>
@@ -79,15 +123,21 @@ export default function HomePage() {
         </div>
       </header>
 
-      <section className="add-padding add-flex ">
+      <section id="about-section" className="add-padding add-flex ">
         <div className="center-text">
           <h1 className="alex-brush">
             <span className="custom-font">Sobre Nós</span>
             <br />
             NOSSA HISTÓRIA
           </h1>
-          <h2>*</h2>
-          <p>todas a historia do rest vai aqui</p>
+          <h4>*</h4>
+          <p>
+            O Restaurante Palatus nasceu do sonho de uma família apaixonada
+            pela culinária e pela arte de bem receber. Fundado em 1995, o
+            restaurante começou como um pequeno negócio familiar, onde receitas
+            tradicionais eram preparadas com amor e dedicação, seguindo os
+            segredos passados de geração em geração.
+          </p>
         </div>
         <div
           className="stuffed-cherries"
@@ -97,40 +147,41 @@ export default function HomePage() {
       </section>
 
       <section className="bread-background center-h1">
-        <h1 className="custom-h1 alex-brush">
-          <span className="custom-font">Receitas</span>
-          <br />
-          De Bom Gosto
-        </h1>
-      </section>
-
-      <section className="menu add-flex add-padding">
-        <div className="menu-images">
-         
-          <img
-            src="https://www.gourmetsociety.co.uk/uploads/images/restaurants/093ce538894c95892f62dc93bb023636-image.png"
-            data-aos="fade-right"
-            data-aos-delay="300"
-            alt="Menu item 3"
-          />
-          
-        </div>
-        <div className="center-text">
+        <div id="menu-section" className="center-text">
           <h1>
             <br />
             MENU
           </h1>
-          <h2>*</h2>
-          <p>
-            Aqui no Palatus, oferecemos os seguintes serviços:
-            <ul>
-              <li>Buffet livre</li>
-              <li>Marmitex Delivery</li>
-              <li>Atendimento a eventos</li>
-            </ul>
-          </p>
-          <a href="#">Serviços</a>
-          <a href="#">Entrar em contato</a>
+          
+
+          <ul>
+            <li>
+              <strong>Buffet Livre:</strong> Desfrute de uma variedade de pratos
+              deliciosos em um ambiente acolhedor.
+            </li>
+            <li>
+              <strong>Marmitas Delivery:</strong> Entregamos refeições frescas e
+              saborosas diretamente na sua casa.
+            </li>
+            <li>
+              <strong>Atendimento a Eventos:</strong> Organizamos serviços
+              personalizados para festas, casamentos e eventos corporativos.
+            </li>
+            <li>
+              <strong>Marmitas para Empresas:</strong> Soluções práticas e
+              econômicas para refeições no ambiente de trabalho.
+            </li>
+          </ul>
+          <div className="menu-actions">
+            <a
+              href="https://wa.me/4797090916"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="menu-button"
+            >
+              Entrar em Contato
+            </a>
+          </div>
         </div>
       </section>
 
@@ -142,6 +193,7 @@ export default function HomePage() {
         </h1>
       </section>
 
+      {/*footer*/}
       <footer>
         <button className="to-top" onClick={handleScrollToTop}>
           <i className="fa fa-chevron-up"></i>TOP
@@ -149,61 +201,47 @@ export default function HomePage() {
 
         <div className="restaurant-info">
           <div className="locations">
-            <h4>LOCATIONS</h4>
+            <h4>Local</h4>
             <div className="location-1">
               <h5>
-                376 Van Brun St
+              R. Alagoas, 11 - Santa Terezinha, 
                 <br />
-                Brooklyn, NY &#8212; 11231
+                Gaspar - SC, 89114-274 
               </h5>
             </div>
-            <div className="location-2">
-              <h5>
-                25 Union Square West
-                <br />
-                New York, NY &#8212; 10003
-              </h5>
-            </div>
+            
           </div>
 
           <div className="hours">
-            <h4>HOURS</h4>
+            <h4>Horário de funcionamento</h4>
             <div className="weekdays">
               <h5>
-                Monday - Thursday
+                Segunda - Sabádo
                 <br />
-                5:30pm - 10:00pm
+                11:00pm - 13:30pm
               </h5>
             </div>
 
-            <div className="weekends">
-              <h5>
-                Friday & Saturday
-                <br />
-                5:30pm - 11:00pm
-              </h5>
-            </div>
+            
 
             <div className="private-events">
               <h5>
-                Available for private
-                <br />
-                events on Sunday
+              Disponível para eventos
+<br />
+privados no domingo
               </h5>
             </div>
           </div>
         </div>
 
         <div className="copyright">
-          <p>
-            <small>
-              Copyright 2014 &#169; Handcrafted with love by <span>PixelGrade</span>{" "}
-              Team
-            </small>
-          </p>
-          <p>
-            <small>Permissions and Copyright &#8226; Contact The Team</small>
-          </p>
+        <small>
+  Copyright 2025 &#169; Restaurante Palatus. Todos os direitos reservados.
+</small>
+<small>
+  Entre em contato conosco para mais informações:{" "}
+  <a href="email@gmail.com">Entre em contato conosco para mais informações:{" "}</a>
+</small>
         </div>
       </footer>
     </>
